736b1058199e45927a1bf5feb5cc4ad00ba020e4
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-11-11 16:39:54 -0300

    project created

---
+++ src/test/java/com/matteo/academicSystem/AcademicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matteo.academicSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
