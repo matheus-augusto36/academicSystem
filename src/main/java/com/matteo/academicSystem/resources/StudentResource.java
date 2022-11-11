93ff362b4ae792dd95e8b66b6f5f6285bdb1f7dc
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-11-11 18:20:04 -0300

    Service layer, component registration

--- src/main/java/com/matteo/academicSystem/resources/StudentResource.java
+++ src/main/java/com/matteo/academicSystem/resources/StudentResource.java
@@ -1,22 +1,34 @@
 package com.matteo.academicSystem.resources;
 
-import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matteo.academicSystem.entities.Student;
+import com.matteo.academicSystem.services.StudentService;
 
 @RestController
 @RequestMapping(value = "/students")
 public class StudentResource {
 	
+	@Autowired
+	private StudentService service;
+	
 	@GetMapping
-	ResponseEntity<Student> findAll() {
-		Student st = new Student(1L, "Geralt of rivia", "123456789", new Date());
-		return ResponseEntity.ok().body(st);
+	public ResponseEntity<List<Student>> findAll() {
+		List<Student> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Student> findById(@PathVariable("id") Long id) {
+		Student obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/matteo/academicSystem/services/StudentService.java
@@ -0,0 +1,26 @@
+package com.matteo.academicSystem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matteo.academicSystem.entities.Student;
+import com.matteo.academicSystem.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentRepository repository;
+	
+	public List<Student> findAll() {
+		return repository.findAll();
+	}
+	
+	public Student findById(Long id) {
+		Optional<Student> obj = repository.findById(id);
+		return obj.get();
+	}
+}
