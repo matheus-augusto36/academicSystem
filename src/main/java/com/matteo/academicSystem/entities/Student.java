6c2834a4391579aff0f2ba7674f8173ccc4769e2
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-11-11 17:11:01 -0300

    H2 database, test profile, JPA

--- src/main/java/com/matteo/academicSystem/entities/Student.java
+++ src/main/java/com/matteo/academicSystem/entities/Student.java
@@ -4,9 +4,17 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
