a184176cefc8d0c20dd7a75af829888117470763
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-12-29 19:42:33 -0300

    Student insert

--- src/main/java/com/matteo/academicSystem/resources/StudentResource.java
+++ src/main/java/com/matteo/academicSystem/resources/StudentResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,16 @@ public class StudentResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Student> findById(@PathVariable("id") Long id) {
 		Student obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Student> insert(@RequestBody Student obj) {
+		/*
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		*/
+		obj = service.insert(obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
