55fa04e28a9ca160c3ac381c630bf9391fa8067e
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2023-06-15 23:03:37 -0300

    CourseClass insert

--- src/main/java/com/matteo/academicSystem/resources/RegistrationResource.java
+++ src/main/java/com/matteo/academicSystem/resources/RegistrationResource.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.matteo.academicSystem.entities.Course;
 import com.matteo.academicSystem.entities.Registration;
 import com.matteo.academicSystem.services.RegistrationService;
 
