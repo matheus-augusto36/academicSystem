ca9486d35a6e3352970121eccbad47b2da5cecfb
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2023-06-16 17:22:17 -0300

    Test delete

--- src/main/java/com/matteo/academicSystem/resources/TestResource.java
+++ src/main/java/com/matteo/academicSystem/resources/TestResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.matteo.academicSystem.entities.Teacher;
 import com.matteo.academicSystem.entities.Test;
 import com.matteo.academicSystem.services.TestService;
 
@@ -43,4 +43,10 @@ public class TestResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}	
 }
--- src/main/java/com/matteo/academicSystem/services/TestService.java
+++ src/main/java/com/matteo/academicSystem/services/TestService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.matteo.academicSystem.entities.Test;
 import com.matteo.academicSystem.repositories.TestRepository;
+import com.matteo.academicSystem.services.exceptions.DatabaseException;
+import com.matteo.academicSystem.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class TestService {
@@ -26,5 +30,15 @@ public class TestService {
 	
 	public Test insert(Test obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
